6d90382bb8fd5e9377dba1087ddef13ee102c3fb
root <root@DESKTOP-FV6EQNU>
2024-09-30 01:10:52 +0300

    eait at .env

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -18,7 +18,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    access_token_expires = timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES","30")))
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
