8c4f22857f2e33bdfa86de940daf51c7c10b1ac2
root <root@DESKTOP-FV6EQNU>
2024-09-30 00:53:49 +0300

    feat: Add /login endpoint

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from datetime import timedelta
+import os
+from fastapi import APIRouter, Depends, HTTPException,status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from ..auth import authenticate_user
 
 router = APIRouter()
 
-# make sure to remove this function
-@router.get("/example")
-def example():
-    return {"message": "this is an example"}
+@router.post("/")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
