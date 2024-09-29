b8babc616d13d1af60bfcb6a5bf8be966c0bbc9b
root <root@DESKTOP-FV6EQNU>
2024-09-30 00:57:34 +0300

    Add create_access_token function

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 
+from datetime import datetime, timedelta, timezone
+import os
+import jwt
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -9,3 +12,14 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm = os.getenv("ALGORITHM"))
+    return encoded_jwt
