2ff00ce76bef3b98a6c5144c3bb7061f01daaf02
root <root@DESKTOP-FV6EQNU>
2024-10-13 10:29:07 +0300

    moved Db_URl to .env file

--- app/connection_to_db.py
+++ app/connection_to_db.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.settings import settings
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:adminpass@db:5432/fastapi_db"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SQLALCHEMY_DATABASE_URL: str
 
     class Config:
         env_file = ".env"  # Specify the .env file to load environment variables from
