0b306c2e513b657db81034ab8b3e02b65904d77b
root <root@DESKTOP-FV6EQNU>
2024-10-16 00:07:47 +0300

    Implement remove status

--- app/services/status_service.py
+++ app/services/status_service.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from uuid import UUID
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status as http_status
-from app.models import Status
+from app.models import Order, Status
 from app.schemas import CreateStatusRequestModel, UpdateStatusRequestModel
 
 
@@ -58,3 +58,24 @@ class StatusService:
         self.db.commit()
         self.db.refresh(status)
         return status
+
+    def remove_status(self, status_id: UUID):
+        # Check if the status exists
+        status = self.db.query(Status).get(status_id)
+        if not status:
+            raise HTTPException(
+                status_code=http_status.HTTP_404_NOT_FOUND,
+                detail="Status not found"
+            )
+
+        # Check if the status is associated with any orders
+        associated_orders = self.db.query(Order).filter(Order.status_id == status_id).first()
+        if associated_orders:
+            raise HTTPException(
+                status_code=http_status.HTTP_400_BAD_REQUEST,
+                detail="Can't delete status. It is used in an order. Consider creating a new status for obsolete items."
+            )
+
+        # If no issues, delete the status
+        self.db.delete(status)
+        self.db.commit()
